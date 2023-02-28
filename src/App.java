04858826b42a841d9bc900d082de9deeba0964be
szakferi <szakfer97@gmail.com>
2023-02-28 20:20:22 +0200

    Added solution to IsPalindrome problem

--- src/App.java
+++ src/App.java
@@ -1,22 +1,40 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("In this app I solve some code challenges");
-        Fizzbuzz();
+        FizzBuzz();
+        IsPalindrome();
     }
 
-    private static void Fizzbuzz() {
+    private static void FizzBuzz() {
         for (int i = 0; i <= 100; i++) {
-            String output="";
+            String output = "";
             if (i % 3 == 0) {
                 output += "Fizz";
             }
             if (i % 5 == 0) {
                 output += "Buzz";
             }
-            if(output == "") {
+            if (output == "") {
                 output = Integer.toString(i);
             }
             System.out.println(output);
         }
+    }
+
+    private static void IsPalindrome() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner. nextLine();
+        String reverseStr = "";
+        int strLength = str.length();
+        for (int i = (strLength - 1); i >= 0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+            System.out.println(str + " is a Palindrome String.");
+        } else {
+            System.out.println(str + " is not a Palindrome String.");
+        }
+        scanner.close();
     }
 }
