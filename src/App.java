aaa5f61d49233e08a9775bd67fac948720fda135
szakferi <szakfer97@gmail.com>
2023-02-28 22:00:49 +0200

    Added solution to IsAnagram problem

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
         System.out.println("In this app I solve some code challenges");
         FizzBuzz();
         IsPalindrome();
+        isAnagram();
     }
 
     private static void FizzBuzz() {
@@ -26,14 +27,32 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         String str = scanner. nextLine();
         String reverseStr = "";
-        int strLength = str.length();
-        for (int i = (strLength - 1); i >= 0; --i) {
+        for (int i = (str.length() - 1); i >= 0; --i) {
             reverseStr = reverseStr + str.charAt(i);
         }
         if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
             System.out.println(str + " is a Palindrome String.");
         } else {
             System.out.println(str + " is not a Palindrome String.");
+        }
+        scanner.close();
+    }
+
+    private static void isAnagram() {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine().toLowerCase();
+        String str2 = scanner.nextLine().toLowerCase();
+        if (str1.length() != str2.length()) {
+            System.out.println("Make sure strings are same length.");
+        }
+        char[] a1 = str1.toCharArray();
+        char[] a2 = str2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        if (Arrays.equals(a1,a2)) {
+            System.out.println("It's an anagram.");
+        } else {
+            System.out.println("Not anagram.");
         }
         scanner.close();
     }
