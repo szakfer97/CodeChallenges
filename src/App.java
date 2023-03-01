5475d08e31bc3652393639d52005f76651545162
szakferi <szakfer97@gmail.com>
2023-03-01 23:04:12 +0200

    Added solution to RomanToInt problem

--- src/App.java
+++ src/App.java
@@ -4,7 +4,8 @@ public class App {
         System.out.println("In this app I solve some code challenges");
         FizzBuzz();
         IsPalindrome();
-        isAnagram();
+        IsAnagram();
+        RomanToInt();
     }
 
     private static void FizzBuzz() {
@@ -25,7 +26,7 @@ public class App {
 
     private static void IsPalindrome() {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner. nextLine();
+        String str = scanner.nextLine();
         String reverseStr = "";
         for (int i = (str.length() - 1); i >= 0; --i) {
             reverseStr = reverseStr + str.charAt(i);
@@ -38,7 +39,7 @@ public class App {
         scanner.close();
     }
 
-    private static void isAnagram() {
+    private static void IsAnagram() {
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine().toLowerCase();
         String str2 = scanner.nextLine().toLowerCase();
@@ -56,4 +57,30 @@ public class App {
         }
         scanner.close();
     }
+
+    public static void RomanToInt() {  
+        Map<Character, Integer> map=new HashMap<Character, Integer>();  
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().toUpperCase();
+        map.put('I',1);  
+        map.put('V',5);  
+        map.put('X',10);  
+        map.put('L',50);  
+        map.put('C',100);  
+        map.put('D',500);  
+        map.put('M',1000);    
+        str = str.replace("IV","IIII");  
+        str = str.replace("IX","VIIII");  
+        str = str.replace("XL","XXXX");  
+        str = str.replace("XC","LXXXX");  
+        str = str.replace("CD","CCCC");  
+        str = str.replace("CM","DCCCC");  
+        int number = 0;  
+        for (int i = 0; i < str.length(); i++)  
+        {  
+            number = number + (map.get(str.charAt(i)));  
+        }  
+        System.out.println("Its int value is: " + number);
+        scanner.close();
+    }  
 }
