99ede46610895bed9e27996599131f10713427ce
szakferi <szakfer97@gmail.com>
2023-03-02 13:54:33 +0200

    Added solution to ReverseBits problem

--- src/App.java
+++ src/App.java
@@ -2,13 +2,9 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("In this app I solve some code challenges");
-        FizzBuzz();
-        IsPalindrome();
-        IsAnagram();
-        RomanToInt();
     }
 
-    private static void FizzBuzz() {
+    public static void FizzBuzz() {
         for (int i = 0; i <= 100; i++) {
             String output = "";
             if (i % 3 == 0) {
@@ -24,7 +20,7 @@ public class App {
         }
     }
 
-    private static void IsPalindrome() {
+    public static void IsPalindrome() {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         String reverseStr = "";
@@ -39,21 +35,21 @@ public class App {
         scanner.close();
     }
 
-    private static void IsAnagram() {
+    public static void IsAnagram() {
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine().toLowerCase();
         String str2 = scanner.nextLine().toLowerCase();
         if (str1.length() != str2.length()) {
-            System.out.println("Make sure strings are same length.");
+            System.out.println(str1 + str2 + "are not the same length.");
         }
         char[] a1 = str1.toCharArray();
         char[] a2 = str2.toCharArray();
         Arrays.sort(a1);
         Arrays.sort(a2);
         if (Arrays.equals(a1,a2)) {
-            System.out.println("It's an anagram.");
+            System.out.println(str2 + "is an anagram of " + str1);
         } else {
-            System.out.println("Not anagram.");
+            System.out.println(str2 + "is not an anagram of " + str1);
         }
         scanner.close();
     }
@@ -78,9 +74,25 @@ public class App {
         int number = 0;  
         for (int i = 0; i < str.length(); i++)  
         {  
-            number = number + (map.get(str.charAt(i)));  
+            number += (map.get(str.charAt(i)));  
         }  
         System.out.println("Its int value is: " + number);
         scanner.close();
     }  
+
+    public static void ReverseBits()
+    {
+        int rev = 0;
+        Scanner scanner = new Scanner(System.in);
+        int nr = Integer.parseInt(scanner.nextLine());
+        while (nr > 0) {
+            rev <<= 1;
+            if ((int)(nr & 1) == 1) {
+                rev ^= 1;
+            }    
+            nr >>= 1;
+        }
+        System.out.println("Number after reversing bits is: " + rev);
+        scanner.close();
+    }
 }
